bcc608f10a0082b48c109a54779557ead32a626a
Suxariki <Artem472008@yandex.ru>
2019-11-16 08:14:33 +0500

    5.8. JavaDoc. Code convention[#198427]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculate;
+
+/**
+ * Class Класс для вычесления арифметическиъ операция + - * / ^
+ * @author Artem
+ * @since 16.11.2019
+ * @version 1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Artyom Mokrushnikov (artem472008@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
