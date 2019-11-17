cdea5462bd7edb7f802be4385d14a6f764be94e0
Artem Mokrushnikov <Artem472008@yandex.ru>
2019-11-17 11:43:29 +0500

    fixing something

--- chapter_001/src/main/java/ru/job4j/ru/job4j/Main.java
+++ chapter_001/src/main/java/ru/job4j/ru/job4j/Main.java
@@ -5,9 +5,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        Date current = new Date(2018, 17, 3);
-        int year = current.getYear();
-        System.out.println("this current year is "+current);
-        boolean job = false;
+
     }
 }
