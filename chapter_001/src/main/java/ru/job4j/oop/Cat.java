5d7a4f823d7f4e09cf61fe5ae906082fa4d9c868
Artem Mokrushnikov <Artem472008@yandex.ru>
2019-11-19 22:07:53 +0500

    1.1. Создание объекта[#198564]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat polkan = new Cat();
+        Cat sharik = new Cat();
+        Cat zhychka = new Cat();
+    }
+}
