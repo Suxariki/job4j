56a3ead4c622bcf37cf6789ace2b8eacba86d7b2
Artem Mokrushnikov <Artem472008@yandex.ru>
2019-11-21 19:15:57 +0500

    2.2. Рефакторинг - Площадь треугольника.[#198560] fix

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -62,9 +62,6 @@ public class Triangle {
      * @return
      */
     private boolean exist(double a, double c, double b) {
-        if ((a + b > c) & (a + c > b) & (b + c > a)) {
-            return true;
-        }
-        else return false;
+        return ((a + b > c) & (a + c > b) & (b + c > a));
     }
 }
