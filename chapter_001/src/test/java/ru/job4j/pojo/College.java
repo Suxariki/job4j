65f6bb2a3506de94f74adca56d058e7911d52f68
Artem Mokrushnikov <Artem472008@yandex.ru>
2019-11-22 20:24:14 +0500

    2. Модель данных.[#198584] fix

--- chapter_001/src/test/java/ru/job4j/pojo/College.java
+++ chapter_001/src/test/java/ru/job4j/pojo/College.java
@@ -5,9 +5,9 @@ import java.util.Date;
 public class College {
     public static void main(String[] args) {
         Student student = new Student();
-        student.setFIO("Mokrushnikov Artem Sergeevich");
+        student.setFio("Mokrushnikov Artem Sergeevich");
         student.setGroup("14b");
         student.setDate(new Date());
-        System.out.println(student.getFIO() + " поступил " + student.getDate() + " в группу " + student.getGroup());
+        System.out.println(student.getFio() + " поступил " + student.getDate() + " в группу " + student.getGroup());
     }
 }
