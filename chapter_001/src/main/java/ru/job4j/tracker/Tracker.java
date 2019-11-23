f7f35e9b485b36e14ecf552a230623a5937b6a81
Artem Mokrushnikov <Artem472008@yandex.ru>
2019-11-23 16:51:34 +0500

    5. Реализовать класс Tracker[#198581] fix

---
+++ chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,132 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @version $Id$
+ * @since 0.1
+ */
+public class Tracker {
+    /**
+     * Массив для хранение заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод, реализующий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод, реализующий удаление заявки по id
+     * @param id id заявки
+     * @return Удалось ли провести операцию
+     */
+
+    public boolean delete(String id) {
+        boolean result = false;
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i] != null && this.items[i].getId().equals(id)) {
+                this.items[i] = null;
+                this.position --;
+                System.arraycopy(this.items,i + 1, this.items, i, this.items.length - i - 1);
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод, реализующий замену заявки на другую по id
+     * @param id id заявки
+     * @param item item для замены
+     * @return Удалось ли провести операцию
+     */
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        int i = indexById(id);
+        if (item != null) {
+            item.setId(id);
+            this.items[i] = item;
+            result = true;
+        }
+        return result;
+    }
+
+
+    /**
+     * Метод, возвращающий массив без null
+     * @return Массив без null
+     */
+
+    public Item[] findAll() {
+        return Arrays.copyOf(this.items, this.position);
+    }
+
+    /**
+     * Метод, возвращающий массив item с одинаковым именем key
+     * @param key
+     * @return Массив с одинаковыми Name
+     */
+
+    public  Item[] findByName(String key) {
+        Item[] result = new Item[this.position];
+        int j = 0;
+        for (int i = 0; i != this.position; i++) {
+            if (items[i].getName().equals(key)) {
+              result[j++] = this.items[i];
+            }
+        }
+        return Arrays.copyOf(result, j);
+    }
+
+    /**
+     * Метод, возвращающий объект с искомым id
+     * @param id
+     * @return Объект с искомым id
+     */
+
+    public Item findById(String id) {
+        int indexResult = indexById(id);
+        return this.items[indexResult];
+    }
+
+    /**
+     * Метод, возвращающий индекс объекста с искомым id
+     * @param id
+     * @return индекс с искомым id
+     */
+
+    public int indexById(String id) {
+        int index = 0;
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getId().equals(id)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,83 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void add() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void findByName() {
+        Tracker tracker = new Tracker();
+        Item test1 = new Item("test1");
+        tracker.add(test1);
+        Item test2 = new Item("test2");
+        tracker.add(test2);
+        Item test3 = new Item("test1");
+        tracker.add(test3);
+        Item[] expected = {test1, test3};
+        Item[] result = tracker.findByName(test1.getName());
+        //сравниваем их
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void findAll() {
+        Tracker tracker = new Tracker();
+        Item test1 = new Item("test1");
+        tracker.add(test1);
+        Item test2 = new Item("test2");
+        tracker.add(test2);
+        Item test3 = new Item("test3");
+        tracker.add(test3);
+        Item[] expected = {test1, test2, test3};
+        Item[] result = tracker.findAll();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenReplaceNameThenReturnNewName() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1");
+        tracker.add(previous);
+        Item next = new Item("test2");
+        tracker.replace(previous.getId(), next);
+        Assert.assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
+    }
+
+    @Test
+    public void whenFirstDeleteThenSecond() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("first");
+        tracker.add(first);
+        Item second = new Item("second");
+        tracker.add(second);
+        tracker.delete(first.getId());
+        Item[] expect = {second};
+        Assert.assertThat(tracker.findAll(), is(expect));
+    }
+
+
+
+    @Test
+    public void findByIdTest() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test");
+        Item third = new Item("test");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        String out = second.getId();
+        assertThat(tracker.findById(out), is(second));
+    }
+}
