5daf793b1c183b63084d2d6d6b197a1e8d3d8a2a
Artem Mokrushnikov <Artem472008@yandex.ru>
2019-11-30 11:50:33 +0500

    9. Написать тесты на StartUI.[#198536]

--- chapter_001/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_001/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,43 +1,20 @@
 package ru.job4j.tracker;
+import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
 
 public class StartUITest {
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
 
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(), // id сохраненной заявки в объект tracker.
-                "replaced item"
-        };
-        StartUI.replaceItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item1 = new Item("первый");
-        tracker.add(item1);
-        String[] answers = {item1.getId()};
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item1.getId());
-        assertThat(deleted, is(nullValue()));
-    }
 }
